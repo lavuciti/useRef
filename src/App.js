974cb7b0205e3589f4a304f64e94baf1744dbe7c
lavuciti <lavrajic@yahoo.com>
2021-06-28 21:58:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Main from "./Main.js";
 export const NameContext = React.createContext();
+export const LastNameContext = React.createContext();
 
+function App() {
 
-class App extends Component {
-
-    state = {
-        name : "Danilo"
-    }
-
-    render(){
+    const [name] = useState("Danilo")
+    const [lastName] = useState("Vesovic")
+ 
         return(
             <div className="container text-center">
-                <NameContext.Provider value={this.state.name}>
-                    <Main name={this.state.name}/>
+                <NameContext.Provider value={name}>
+                    <LastNameContext.Provider value={lastName}>
+                        <Main />
+                    </LastNameContext.Provider> 
                 </NameContext.Provider>
-                
             </div>
         )
-    }
 }
 
 export default App;
 
- /*ako se ne stavi exact tada kada se klikne na about vidi se i home page i about page*/
--- src/Aside.js
+++ src/Aside.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import {NameContext} from "./App"
+import React,{useContext} from 'react';
+import {NameContext, LastNameContext} from "./App"
 
 function Asaide(){
+
+    const name = useContext(NameContext);
+    const lastName = useContext(LastNameContext);
+
     return(
         <div>
-            <NameContext.Consumer>
-                {
-                    name =>{
-                        return(
-                            <h1>Aside name : {name}</h1>
-                        )
-                    }
-                }
-            </NameContext.Consumer>
+            <h1>Aside && name : {name} {lastName}</h1>
         </div>
     )
 }
